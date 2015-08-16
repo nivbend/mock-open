07400a6f328d275a1a0f45a0f24d1781c29d9067
Niv Ben-David <nivbend@gmail.com>
2015-08-16 11:35:54 +0300

    Allow access to file mocks with a mapping interface
    Also moved the `read_data` property to the FileLikeMock class instead of
    OpenMock.

--- mock_open/mock_open.py
+++ mock_open/mock_open.py
@@ -10,15 +10,8 @@ class FileLikeMock(NonCallableMock):
     def __init__(self, read_data="", *args, **kws):
         kws.update({"spec": file, })
         super(FileLikeMock, self).__init__(*args, **kws)
-        self.__contents = StringIO()
         self.__is_closed = False
-
-        # Constructing a cStrinIO object with the input string would result
-        # in a read-only object, so we write the contents after construction.
-        self.__contents.write(read_data)
-
-        # Set tell/read/write/etc side effects to defaults.
-        self.reset_mock()
+        self.read_data = read_data
 
     @property
     def closed(self):
@@ -27,6 +20,21 @@ class FileLikeMock(NonCallableMock):
     @property
     def read_data(self):
         return self.__contents.getvalue()
+
+    @read_data.setter
+    def read_data(self, contents):
+        self.__contents = StringIO()
+
+        # Constructing a cStrinIO object with the input string would result
+        # in a read-only object, so we write the contents after construction.
+        self.__contents.write(contents)
+
+        # Set tell/read/write/etc side effects to access the new contents
+        # object.
+        self.tell.side_effect = self.__contents.tell
+        self.read.side_effect = self.__contents.getvalue
+        self.write.side_effect = self.__contents.write
+        self.close.side_effect = self._close
 
     def __enter__(self):
         return self
@@ -38,10 +46,8 @@ class FileLikeMock(NonCallableMock):
         """Reset the default tell/read/write/etc side effects."""
         super(FileLikeMock, self).reset_mock(visited)
 
-        self.tell.side_effect = self.__contents.tell
-        self.read.side_effect = self.__contents.getvalue
-        self.write.side_effect = self.__contents.write
-        self.close.side_effect = self._close
+        # Reset contents and tell/read/write/close side effects.
+        self.read_data = ""
 
     def _close(self):
         self.__is_closed = True
@@ -51,22 +57,32 @@ class MockOpen(Mock):
     def __init__(self, read_data="", *args, **kws):
         kws.update({"spec": open, "name": open.__name__, })
         super(MockOpen, self).__init__(*args, **kws)
+        self.__files = {}
         self.__read_data = read_data
 
     def __call__(self, path, mode="r", *args, **kws):
+        if path in self.__files:
+            self.return_value = self.__files[path]
+            self.side_effect = self.return_value.side_effect
+
         child = super(MockOpen, self).__call__(path, mode, *args, **kws)
         child.name = path
         # pylint: disable=attribute-defined-outside-init
         child.mode = mode
+
+        if path not in self.__files:
+            self.__files[path] = child
+
         return child
 
-    @property
-    def read_data(self):
-        return self.__read_data
+    def __getitem__(self, path):
+        return self.__files.setdefault(path, FileLikeMock())
 
-    @read_data.setter
-    def read_data(self, data):
-        self.__read_data = data
+    def __setitem__(self, path, value):
+        value.__enter__ = lambda self: self
+        value.__exit__ = lambda self, *args: None
+        self.__files[path] = value
 
     def _get_child_mock(self, **kws):
-        return FileLikeMock(read_data=self.__read_data, **kws)
+        kws.update({"read_data": self.__read_data, })
+        return FileLikeMock(**kws)
--- mock_open/test/test_mock_open.py
+++ mock_open/test/test_mock_open.py
@@ -1,6 +1,6 @@
 import unittest
-from mock import patch, call
-from mock_open import MockOpen
+from mock import patch, call, NonCallableMock
+from ..mock_open import MockOpen, FileLikeMock
 from functools import wraps
 
 
@@ -106,8 +106,8 @@ class TestOpenSingleFiles(unittest.TestCase):
         ]
 
         # We even allow adding contents to the file incrementally.
-        mock_open.read_data = "\n".join(contents[:-1])
-        mock_open.read_data += "\n" + contents[-1]
+        mock_open.return_value.read_data = "\n".join(contents[:-1])
+        mock_open.return_value.read_data += "\n" + contents[-1]
 
         with open("/path/to/file", "r") as handle:
             data = handle.read()
@@ -116,6 +116,77 @@ class TestOpenSingleFiles(unittest.TestCase):
         handle.write.assert_not_called()
         handle.read.assert_called_once_with()
         self.assertEquals("\n".join(contents), data)
+
+    def test_getitem_after_call(self, mock_open):
+        """Retrieving a handle after the call to open() should give us the same
+        object.
+        """
+        with open("/path/to/file", "r") as handle:
+            pass
+
+        self.assertEquals(handle, mock_open["/path/to/file"])
+
+    def test_setting_custom_mock(self, mock_open):
+        """Check 'manually' setting a mock for a file."""
+        custom_mock = NonCallableMock()
+        mock_open["/path/to/file"] = custom_mock
+
+        # Make sure other files aren't affected.
+        self.assertIsInstance(open("/path/to/other_file", "r"), FileLikeMock)
+
+        # Check with a regular call.
+        self.assertEquals(custom_mock, open("/path/to/file", "r"))
+
+        # Check as a context manager.
+        custom_mock.read.side_effect = IOError()
+        custom_mock.write.side_effect = IOError()
+        with open("/path/to/file") as handle:
+            self.assertIs(custom_mock, handle)
+            self.assertRaises(IOError, handle.read)
+            self.assertRaises(IOError, handle.write, "error")
+
+
+@patch("__builtin__.open", new_callable=MockOpen)
+class TestMultipleCalls(unittest.TestCase):
+    """Test multiple calls to open()."""
+    def test_read_then_write(self, mock_open):
+        """Accessing the same file should handle the same object.
+
+        Reading from a file after writing to it should give us the same
+        contents.
+        """
+        with open("/path/to/file", "w") as first_handle:
+            first_handle.write("Ground control to Major Tom")
+
+        with open("/path/to/file", "r") as second_handle:
+            contents = second_handle.read()
+
+        self.assertEquals(first_handle, second_handle)
+        self.assertEquals("Ground control to Major Tom", contents)
+
+    def test_access_different_files(self, mock_open):
+        """Check access to different files with multiple calls to open()."""
+        first_handle = mock_open["/path/to/first_file"]
+        second_handle = mock_open["/path/to/second_file"]
+
+        first_handle.read_data = "This is the FIRST file"
+        second_handle.read_data = "This is the SECOND file"
+
+        with open("/path/to/first_file", "r") as handle:
+            self.assertEquals("This is the FIRST file", handle.read())
+
+        with open("/path/to/second_file", "r") as handle:
+            self.assertEquals("This is the SECOND file", handle.read())
+
+        # return_value is set to the last handle returned.
+        self.assertEquals(second_handle, mock_open.return_value)
+
+        self.assertEquals("/path/to/first_file", first_handle.name)
+        self.assertEquals("r", first_handle.mode)
+        self.assertEquals("/path/to/second_file", second_handle.name)
+        self.assertEquals("r", second_handle.mode)
+        first_handle.read.assert_called_once_with()
+        second_handle.read.assert_called_once_with()
 
 
 @patch("__builtin__.open", new_callable=MockOpen)
@@ -142,6 +213,25 @@ class TestSideEffects(unittest.TestCase):
         with open("/path/to/file", "r") as handle:
             with self.assertRaises(IOError):
                 handle.write("text")
+
+    def test_error_by_name(self, mock_open):
+        """Raise an exception for a specific path."""
+        mock_open["/path/to/error_file"].side_effect = IOError()
+
+        # Trying to open a different file should be OK.
+        with open("/path/to/allowed_file", "r") as allowed_handle:
+            pass
+
+        # But openning the bad file should raise an exception.
+        self.assertRaises(IOError, open, "/path/to/error_file", "r")
+
+        # Reset open's side effect and check read/write side effects.
+        mock_open["/path/to/error_file"].side_effect = None
+        mock_open["/path/to/error_file"].read.side_effect = IOError()
+        mock_open["/path/to/error_file"].write.side_effect = IOError()
+        with open("/path/to/error_file", "r") as handle:
+            self.assertRaises(IOError, handle.read)
+            self.assertRaises(IOError, handle.write, "Bad write")
 
     def test_hijack_read(self, mock_open):
         """Replace the normal read() with a fake one.
@@ -196,7 +286,7 @@ class TestSideEffects(unittest.TestCase):
 
         # Avoid uninitialized assignment (see test_hijack_write()).
         sentinal = [False, ]
-        mock_open.read_data = "Some text"
+        mock_open.return_value.read_data = "Some text"
         wrap_read(mock_open.return_value.read)
 
         with open("/path/to/file", "w") as handle:

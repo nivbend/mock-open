5ceb37497484ae15ca0e93a953cf3509334adf3b
Niv Ben-David <nivbend@gmail.com>
2015-08-17 22:17:42 +0300

    Add tox configuration to project
    Support python 2.7, 3.2, 3.3, 3.4

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
     ],
     keywords=" ".join([
